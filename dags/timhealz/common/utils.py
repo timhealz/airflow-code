c22ab546ea347c0749b0e2530da179cd3f27a031
timhealz <healyt22@gmail.com>
2021-12-30 13:27:32 -0800

    [spendy] updates to run 7 days everyday

--- dags/timhealz/common/utils.py
+++ dags/timhealz/common/utils.py
@@ -1,3 +1,5 @@
+import airflow.macros as macros
+
 from airflow.hooks.base import BaseHook
 from sqlalchemy import create_engine
 
@@ -10,3 +12,13 @@ def get_mysql_db_engine():
     )
 
     return db_engine
+
+
+def mint_date_to_ds(mint_date: str) -> str:
+    try:
+        month, day = mint_date.split()
+        ds = macros.ds_format(f"2021-{month}-{day}","%Y-%b-%d", "%Y-%m-%d")
+    except:
+        ds = macros.ds_format(mint_date, "%m/%d/%y", "%Y-%m-%d")
+    
+    return ds
